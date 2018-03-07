148dfd6faea6d76f993be6aa9ede419204799bc5
chamer81 <chanson.exmormon@gmail.com>
2018-03-07 17:42:58 +0100

    parse the html mustache output as a react element

--- index.js
+++ index.js
@@ -6,16 +6,28 @@ const ReactDOMServer = require('react-dom/server')
 const fs = require('fs')
 const http = require('http')
 
-const test_template = fs.readFileSync('templates/tiny.mustache', 'utf-8')
+const Parser = require('html-react-parser')
+
+const test_template = fs.readFileSync(
+  'templates/confirmation-email.mustache',
+  'utf-8'
+)
+const test_snippet = fs.readFileSync('templates/snippet-test.mustache', 'utf-8')
 console.log(test_template)
-const test_view = JSON.parse(fs.readFileSync('data/test_view.json', 'utf-8'))
+const test_view = JSON.parse(
+  fs.readFileSync('data/confirmation-email.json', 'utf-8')
+)
 console.log(test_view)
 
 const TestComponentContainingMustache = React.createElement(
   'div',
   null,
-  React.createElement('h1', null, Mustache.render('{{title}}', test_view)),
-  React.createElement('p', null, Mustache.render(test_template, test_view))
+  React.createElement(
+    'h1',
+    null,
+    Mustache.render('{{title}}', {title: 'This is a test.'})
+  ),
+  React.createElement('p', null, Parser('<p>inner paragraph</p>', 'text/xml'))
 )
 
 const output = ReactDOMServer.renderToStaticMarkup(
@@ -26,7 +38,10 @@ console.log(output)
 
 http
   .createServer(function(req, res) {
-    fs.readFile('templates/tiny.mustache', 'utf-8', function(err, data) {
+    fs.readFile('templates/confirmation-email.mustache', 'utf-8', function(
+      err,
+      data
+    ) {
       res.writeHead(200, {'Content-Type': 'text/html'})
       res.write(
         ReactDOMServer.renderToStaticMarkup(
@@ -36,9 +51,17 @@ http
             React.createElement(
               'h1',
               null,
-              Mustache.render('{{title}}', test_view)
+              Mustache.render('{{title}}', {title: 'This is a test.'})
             ),
-            React.createElement('p', null, Mustache.render(data, test_view))
+            React.createElement(
+              'p',
+              null,
+              Parser(
+                Mustache.render(data, test_view, {
+                  testsnippet: test_snippet
+                })
+              )
+            )
           )
         )
       )
