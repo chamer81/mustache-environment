8c5b18daa9612f5a75a3d94eef0a266edd582b88
chamer81 <chanson.exmormon@gmail.com>
2018-03-08 13:00:58 +0100

    use real report data

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-const React = require('react')
-
 const Mustache = require('mustache')
-const ReactDOMServer = require('react-dom/server')
 
 const fs = require('fs')
 const http = require('http')
-
-const Parser = require('html-react-parser')
 
 const filenames = fs.readdirSync('snippets')
 
@@ -27,51 +22,12 @@ const template_name =
 const template = fs.readFileSync('templates/' + template_name, 'utf-8')
 //console.log(test_template)
 
-const test_view = JSON.parse(
-  fs.readFileSync('data/confirmation-email.json', 'utf-8')
-)
+const test_view = JSON.parse(fs.readFileSync('data/report-data.json', 'utf-8'))
 //console.log(test_view)
-
-const TestComponentContainingMustache = React.createElement(
-  'div',
-  null,
-  React.createElement(
-    'h1',
-    null,
-    Mustache.render('{{title}}', {title: 'This is a test.'})
-  ),
-  React.createElement('p', null, Parser('<p>inner paragraph</p>', 'text/xml'))
-)
-
-const output = ReactDOMServer.renderToStaticMarkup(
-  TestComponentContainingMustache
-)
-
-//console.log(output)
 
 http
   .createServer(function(req, res) {
-    fs.readFile('templates/' + template_name, 'utf-8', function(err, data) {
-      res.writeHead(200, {'Content-Type': 'text/html'})
-      res.write(
-        ReactDOMServer.renderToStaticMarkup(
-          React.createElement(
-            'div',
-            null,
-            React.createElement(
-              'h1',
-              null,
-              Mustache.render('{{title}}', {title: 'This is a test.'})
-            ),
-            React.createElement(
-              'p',
-              null,
-              Parser(Mustache.render(template, test_view, snippets))
-            )
-          )
-        )
-      )
-      res.end()
-    })
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    res.write(Mustache.render(template, test_view, snippets))
   })
   .listen(8080)
